2744ed592254125c89f318d96f6e3400bd7a6195
Arbialexbot <alexpao@hotmail.it>
2025-04-26 22:19:53 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,23 @@
+import time
 from binance.client import Client
 
-# Inserisci qui le tue API Key e Secret
+# Inserisci le tue API Key e Secret
 api_key = 'G9YPk5NuUDaDBs1VnqsFiwBBQpmlJKzPQqN2fwgVi1iOUpG7mqiON4m1H5caFUJA'
 api_secret = 'Qqtq5ihtsvBk97j4MkorZD2G7wFMPoxuFrkoGC3GbpS25Xr1P8KnxdSCK4upL2IC'
 
 # Connessione a Binance
 client = Client(G9YPk5NuUDaDBs1VnqsFiwBBQpmlJKzPQqN2fwgVi1iOUpG7mqiON4m1H5caFUJA, Qqtq5ihtsvBk97j4MkorZD2G7wFMPoxuFrkoGC3GbpS25Xr1P8KnxdSCK4upL2IC)
 
-# Controlliamo il saldo dell'account
-account_info = client.get_account()
-print("Saldo disponibile:")
-for balance in account_info['balances']:
-    if float(balance['free']) > 0:
-        print(f"{balance['asset']}: {balance['free']}")
+# Funzione che legge e stampa il saldo
+def mostra_saldo():
+    account_info = client.get_account()
+    print("\nSaldo disponibile:")
+    for balance in account_info['balances']:
+        if float(balance['free']) > 0:
+            print(f"{balance['asset']}: {balance['free']}")
+
+# Ciclo infinito: controlla ogni 30 secondi
+while True:
+    mostra_saldo()
+    print("Aspetto 30 secondi...\n")
+    time.sleep(30)
