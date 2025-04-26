5b6542885e90b28ceea98439163f0deaddbd760d
Arbialexbot <alexpao@hotmail.it>
2025-04-26 22:40:35 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,11 +2,11 @@ import time
 from binance.client import Client
 
 # Inserisci le tue API Key e Secret
-api_key = 'G9YPk5NuUDaDBs1VnqsFiwBBQpmlJKzPQqN2fwgVi1iOUpG7mqiON4m1H5caFUJA'
-api_secret = 'Qqtq5ihtsvBk97j4MkorZD2G7wFMPoxuFrkoGC3GbpS25Xr1P8KnxdSCK4upL2IC'
+api_key = "G9YPk5NuUDaDBs1VnqsFiwBBQpmlJKzPQqN2fwgVi1iOUpG7mqiON4m1H5caFUJA"
+api_secret = "Qqtq5ihtsvBk97j4MkorZD2G7wFMPoxuFrkoGC3GbpS25Xr1P8KnxdSCK4upL2IC"
 
 # Connessione a Binance
-client = Client(G9YPk5NuUDaDBs1VnqsFiwBBQpmlJKzPQqN2fwgVi1iOUpG7mqiON4m1H5caFUJA, Qqtq5ihtsvBk97j4MkorZD2G7wFMPoxuFrkoGC3GbpS25Xr1P8KnxdSCK4upL2IC)
+client = Client("G9YPk5NuUDaDBs1VnqsFiwBBQpmlJKzPQqN2fwgVi1iOUpG7mqiON4m1H5caFUJA", "Qqtq5ihtsvBk97j4MkorZD2G7wFMPoxuFrkoGC3GbpS25Xr1P8KnxdSCK4upL2IC")
 
 # Funzione che legge e stampa il saldo
 def mostra_saldo():
